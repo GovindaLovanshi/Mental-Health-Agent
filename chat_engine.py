64bd77eb3f8a806c0eb3aa3b6172fc459cd81c3f
Govinda Lovanshi <govindalovanshi417@gmail.com>
2025-06-06 14:06:43 +0530

    this is the first component of project

---
+++ chat_engine.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
